933e719ba7fc7bb4e2937ef7d9da1bad0bbf6c0f
Gabriel Paim <50223173+gabrielPaim98@users.noreply.github.com>
2022-05-07 19:02:11 -0300

    Added images to list

--- Android/app/src/main/java/br/ucsal/tcc_android/ListActivity.kt
+++ Android/app/src/main/java/br/ucsal/tcc_android/ListActivity.kt
@@ -4,24 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import org.json.JSONArray
 
 class ListActivity : AppCompatActivity() {
-    private var mList = List(1000) { "item: $it" }
+//    private var mList = List(1000) { "item: $it" }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+        val mList = mutableListOf<ListData>()
+        val jsonArray = JSONArray(loadJSONFromAssets("MOCK_DATA.json"))
+        for (i in 0 until jsonArray.length()) {
+            val data = ListData()
+            val jsonObject = jsonArray.getJSONObject(i)
+            data.first_name = jsonObject.getString("first_name")
+            data.image_url = jsonObject.getString("image_url")
+            mList.add(data)
+        }
 
         findViewById<RecyclerView>(R.id.recycler_view).adapter = RVAdapter(mList)
     }
+
+    private fun loadJSONFromAssets(fileName: String): String {
+        return this.assets.open(fileName).bufferedReader().use { reader ->
+            reader.readText()
+        }
+    }
 }
 
-class RVAdapter(private val dataSet: List<String>) :
+class RVAdapter(private val dataSet: List<ListData>) :
     ListAdapter<String, RVAdapter.ViewHolder>(DiffCallback) {
     companion object DiffCallback : DiffUtil.ItemCallback<String>() {
         override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
@@ -35,6 +53,7 @@ class RVAdapter(private val dataSet: List<String>) :
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.list_item_tv)
+        val imageView: ImageView = view.findViewById(R.id.avatar)
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
@@ -45,7 +64,13 @@ class RVAdapter(private val dataSet: List<String>) :
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.textView.text = dataSet[position]
+        val data = dataSet[position]
+        viewHolder.textView.text = data.first_name
+        viewHolder.imageView.apply {
+            Glide.with(context)
+                .load(data.image_url)
+                .into(this)
+        }
     }
 
     override fun getItemCount() = dataSet.size
---
+++ Android/app/src/main/java/br/ucsal/tcc_android/ListData.kt
@@ -0,0 +1,8 @@
+package br.ucsal.tcc_android
+
+import java.io.Serializable
+
+class ListData: Serializable {
+    var first_name: String = ""
+    var image_url: String = ""
+}
